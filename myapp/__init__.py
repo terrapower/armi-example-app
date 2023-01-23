5df2055b72a7bbdf0e124d1ed3f66a210faaef00
John Stilley <1831479+john-science@users.noreply.github.com>
2023-01-23 15:25:55 -0800

    Version bump: various updates and bug fixes (#5)

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -4,4 +4,4 @@ Example ARMI Application.
 Code here runs whenever you import anything in this package.
 """
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
--- myapp/app.py
+++ myapp/app.py
@@ -1,21 +1,34 @@
 """Application module"""
 
+from armi import __version__ as armi_version
 from armi.apps import App
 
+from myapp import __version__ as myapp_version
 from myapp.plugin import DummyPhysicsPlugin
 
 
 class ExampleApp(App):
+    name = "example-app"
+
     def __init__(self):
         # activate all built-in plugins
         App.__init__(self)
-
-        # (optionally) set the name class attribute
-        name = "example-app"
 
         # register our plugin with the plugin manager
         self._pm.register(DummyPhysicsPlugin)
 
     @property
     def splashText(self):
-        return "** My Example App **"
+        return f"""                           
++===============================================+
++                                               +
++                 |\/|   /|                     +
++                 |  |\//-||)|)                 +
++                     /    | |                  +
++                                               +
++                Version: {myapp_version}                 +
++                                               +
++-----------------------------------------------+
++    Powered by ARMI (R) -- Version: {armi_version}      +
++===============================================+
+"""
