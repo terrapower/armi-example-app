60becb5137cf3c3671ebd44b06b894287611c181
jstilley <jstilley@terrapower.com>
2023-05-24 15:03:58 -0700

    Updating to ARMI v0.2.7 settings versions

--- myapp/plugin.py
+++ myapp/plugin.py
@@ -1,7 +1,9 @@
 from armi import interfaces
 from armi import plugins
 from armi.interfaces import STACK_ORDER as ORDER
+from armi.settings.fwSettings.globalSettings import CONF_VERSIONS
 
+from myapp import __version__
 from myapp import fluxSolver
 from myapp import settings
 from myapp import thermalSolver
@@ -11,6 +13,8 @@ class DummyPhysicsPlugin(plugins.ArmiPlugin):
     @staticmethod
     @plugins.HOOKIMPL
     def exposeInterfaces(cs):
+        DummyPhysicsPlugin.setVersionInSettings(cs)
+
         kernels = [
             interfaces.InterfaceInfo(ORDER.FLUX, fluxSolver.FluxInterface, {}),
             interfaces.InterfaceInfo(
@@ -28,3 +32,13 @@ class DummyPhysicsPlugin(plugins.ArmiPlugin):
     @plugins.HOOKIMPL
     def defineSettingsValidators(inspector):
         return settings.defineValidators(inspector)
+
+    @staticmethod
+    @plugins.HOOKIMPL
+    def defineCaseDependencies(case, suite):
+        DummyPhysicsPlugin.setVersionInSettings(case.cs)
+
+    @staticmethod
+    def setVersionInSettings(cs):
+        """Helper method to set the version correctly in the Settings file."""
+        cs[CONF_VERSIONS]["armi-example-app"] = __version__
--- myapp/settings.py
+++ myapp/settings.py
@@ -3,12 +3,9 @@
 from armi.operators.settingsValidation import Query, validateVersion
 from armi.settings import setting
 
-from myapp import __version__ as version
-
 
 CONF_INLET_TEMPERATURE = "inletInC"
 CONF_OUTLET_TEMPERATURE = "outletInC"
-CONF_MYAPP_VERSION = "myAppVersion"
 
 
 def defineSettings():
@@ -26,12 +23,6 @@ def defineSettings():
             label="Outlet Temperature (Celcius)",
             description="The Outlet Temperature in C, for the TH solver.",
         ),
-        setting.Setting(
-            CONF_MYAPP_VERSION,
-            default="uncontrolled",
-            label="Acceptable version of this app.",
-            description="Valid versions of this app, that this settings file will accept.",
-        ),
     ]
     return settings
 
@@ -48,12 +39,6 @@ def defineValidators(inspector):
         Query(
             lambda: inspector.cs[CONF_OUTLET_TEMPERATURE] < 0.0,
             "The outlet temperature is below 0. This is unphysical and will result in unphysical results.",
-            "",
-            inspector.NO_ACTION,
-        ),
-        Query(
-            lambda: not validateVersion(version, inspector.cs[CONF_MYAPP_VERSION]),
-            "The current application version does not match the required version in the Settings file! This run is uncontrolled!",
             "",
             inspector.NO_ACTION,
         ),
